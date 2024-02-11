1a952deac017823462574775c0df6ac29fbeb56d
SyedAzeemJaved <syedazeemjaved@gmail.com>
2024-02-12 04:27:39 +0500

    Setup expo-network

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
-import { useCallback } from 'react';
+import { useEffect, useCallback } from 'react';
 
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+
+import * as Network from 'expo-network';
 
 import * as SplashScreen from 'expo-splash-screen';
 import { useFonts } from 'expo-font';
@@ -14,6 +16,26 @@ import { Navigation } from '@navigations';
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+    useEffect(() => {
+        (async () => {
+            const pr = await Network.getNetworkStateAsync();
+            console.log({
+                pr,
+            });
+
+            const apiResponse = await fetch(
+                'https://firebasestorage.googleapis.com/v0/b/connect2mark-3cebc.appspot.com/o/c943801f-8cdc-4307-8407-60a507e57f6e?alt=media&token=2ff8826c-1069-44f4-b9ea-f562d7f9c0f4',
+                { method: 'GET' }
+            );
+
+            if (apiResponse.ok) {
+                console.log('Worked');
+            } else {
+                console.log('Did not work');
+            }
+        })();
+    }, []);
+
     const [fontsLoaded] = useFonts({
         Poppins: require('./assets/fonts/Poppins/Poppins-Regular.ttf'),
         MarcellusSc: require('./assets/fonts/Marcellus_SC/MarcellusSC-Regular.ttf'),
@@ -29,13 +51,13 @@ export default function App() {
     }
 
     return (
-         <GestureHandlerRootView style={{ flex: 1 }}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
             <AuthProvider>
                 <NavigationContainer onReady={onLayoutRootView}>
                     <Navigation />
                 </NavigationContainer>
             </AuthProvider>
             <Toast />
-        </ GestureHandlerRootView>
+        </GestureHandlerRootView>
     );
 }
